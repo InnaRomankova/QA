f0bbc43fa3eb5b40973503270a6fe913df09d2f5
Inna Romankova <romankova03071978@gmail.com>
2020-04-04 20:46:36 +0300

    added Task2_2 day1

---
+++ src/day1/Task2_2.java
@@ -0,0 +1,12 @@
+package day1;
+/*2. Используя while вывести все числа от 0 до 25 в одну строку через пробел.*/
+
+public class Task2_2 {
+    public static void main(String[] args) {
+        int number = 0;
+        while (number <= 25) {
+            System.out.print (number + " ");
+            number++;
+        }
+    }
+}
