32baa1ce428c2a43402d75dd7f894110b0febb59
Inna Romankova <romankova03071978@gmail.com>
2020-04-11 18:35:13 +0300

    class Runner day4 is added

---
+++ src/day4/Runner.java
@@ -0,0 +1,21 @@
+package day4;
+
+import day4.entity.AutomatedTest;
+import day4.entity.ManualTest;
+import day4.entity.Test;
+import day4.entity.TestLevel;
+import day4.worker.AutomationEngineer;
+import day4.worker.Engineer;
+import day4.worker.TestEngineer;
+
+public class Runner {
+    public static void main(String[] args) {
+
+        Engineer testEngineer = new TestEngineer ();
+        Engineer automationEngineer = new AutomationEngineer ();
+        Test manualTest = new ManualTest (TestLevel.GUI, 4);
+        Test automatedTest = new AutomatedTest (TestLevel.API, 1);
+        System.out.println ("Result execute test1: " + manualTest.apply (automationEngineer));
+        System.out.println ("Result execute test2: " + automatedTest.apply (testEngineer));
+    }
+}
